943d0fa5a3cd83b46c7966edd69d886a9d86ea35
zxh326 <zhangxh1997@gmail.com>
2017-10-15 22:50:03 +0800

    add crontab

---
+++ cron.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env/python
+'''
+暂时支持linux
+windows 暂不支持
+'''
+
+import sys
+
+from crontab import CronTab,time
+
+def addCron():
+	my_user_cron  = CronTab(user=True)
+
+	command = sys.path[2] + ' ' + sys.path[0] + '/cron.py >> /var/log/zhihudaily.log'
+
+	job = my_user_cron.new(command=command)
+
+	job.setall(time(8,10))
+
+	my_user_cron.write()
+
+
+if __name__ == '__main__':
+	addCron()
