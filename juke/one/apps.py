cbc8ab6700b4d492a6351b1cdeac7f8e56451c41
zxh326 <zhangxh1997@gmail.com>
2017-10-31 19:34:20 +0800

    deleted:    juke/juke/__init__.py 	deleted:    juke/juke/settings.py 	deleted:    juke/juke/urls.py 	deleted:    juke/juke/wsgi.py 	deleted:    juke/manage.py 	deleted:    juke/one/__init__.py 	deleted:    juke/one/admin.py 	deleted:    juke/one/apps.py 	deleted:    juke/one/getdetail.py 	deleted:    juke/one/models.py 	deleted:    juke/one/tests.py 	deleted:    juke/one/views.py 	deleted:    juke/static/admin/css/base.css 	deleted:    juke/static/admin/css/changelists.css 	deleted:    juke/static/admin/css/dashboard.css 	deleted:    juke/static/admin/css/fonts.css 	deleted:    juke/static/admin/css/forms.css 	deleted:    juke/static/admin/css/login.css 	deleted:    juke/static/admin/css/rtl.css 	deleted:    juke/static/admin/css/widgets.css 	deleted:    juke/static/admin/fonts/LICENSE.txt 	deleted:    juke/static/admin/fonts/README.txt 	deleted:    juke/static/admin/fonts/Roboto-Bold-webfont.woff  	deleted:    juke/static/admin/fonts/Roboto-Light-webfont.woff 	deleted:    juke/static/admin/fonts/Roboto-Regular-webfont.woff 	deleted:    juke/static/admin/img/LICENSE 	deleted:    juke/static/admin/img/README.txt 	deleted:    juke/static/admin/img/calendar-icons.svg 	deleted:    juke/static/admin/img/gis/move_vertex_off.svg 	deleted:    juke/static/admin/img/gis/move_vertex_on.svg 	deleted:    juke/static/admin/img/icon-addlink.svg 	deleted:    juke/static/admin/img/icon-alert.svg 	deleted:    juke/static/admin/img/icon-calendar.svg 	deleted:    juke/static/admin/img/icon-changelink.svg 	deleted:    juke/static/admin/img/icon-clock.svg 	deleted:    juke/static/admin/img/icon-deletelink.svg 	deleted:    juke/static/admin/img/icon-no.svg 	deleted:    juke/static/admin/img/icon-unknown-alt.svg 	deleted:    juke/static/admin/img/icon-unknown.svg 	deleted:    juke/static/admin/img/icon-yes.svg 	deleted:    juke/static/admin/img/inline-delete.svg 	deleted:    juke/static/admin/img/search.svg 	deleted:    juke/static/admin/img/selector-icons.svg 	deleted:    juke/static/admin/img/sorting-icons.svg 	deleted:    juke/static/admin/img/tooltag-add.svg 	deleted:    juke/static/admin/img/tooltag-arrowright.svg 	deleted:    juke/static/admin/js/SelectBox.js 	deleted:    juke/static/admin/js/SelectFilter2.js 	deleted:    juke/static/admin/js/actions.js 	deleted:    juke/static/admin/js/actions.min.js 	deleted:    juke/static/admin/js/admin/DateTimeShortcuts.js 	deleted:    juke/static/admin/js/admin/RelatedObjectLookups.js 	deleted:    juke/static/admin/js/calendar.js 	deleted:    juke/static/admin/js/cancel.js 	deleted:    juke/static/admin/js/change_form.js 	deleted:    juke/static/admin/js/collapse.js 	deleted:    juke/static/admin/js/collapse.min.js 	deleted:    juke/static/admin/js/core.js 	deleted:    juke/static/admin/js/inlines.js 	deleted:    juke/static/admin/js/inlines.min.js 	deleted:    juke/static/admin/js/jquery.init.js 	deleted:    juke/static/admin/js/popup_response.js 	deleted:    juke/static/admin/js/prepopulate.js 	deleted:    juke/static/admin/js/prepopulate.min.js 	deleted:    juke/static/admin/js/prepopulate_init.js 	deleted:    juke/static/admin/js/timeparse.js 	deleted:    juke/static/admin/js/urlify.js 	deleted:    juke/static/admin/js/vendor/jquery/LICENSE-JQUERY.txt 	deleted:    juke/static/admin/js/vendor/jquery/jquery.js 	deleted:    juke/static/admin/js/vendor/jquery/jquery.min.js 	deleted:    juke/static/admin/js/vendor/xregexp/LICENSE-XREGEXP.txt 	deleted:    juke/static/admin/js/vendor/xregexp/xregexp.js 	deleted:    juke/static/admin/js/vendor/xregexp/xregexp.min.js 	deleted:    juke/zhihu/__init__.py 	deleted:    juke/zhihu/admin.py 	deleted:    juke/zhihu/apps.py 	deleted:    juke/zhihu/getdetail.py 	deleted:    juke/zhihu/models.py 	deleted:    juke/zhihu/tests.py 	deleted:    juke/zhihu/views.py 	juke/ 	manage.py 	one/ 	static/ 	zhihu/

--- juke/one/apps.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-from django.apps import AppConfig
-
-
-class OneConfig(AppConfig):
-    name = 'one'
-class urlConfig():
-	Baseurl = 'http://m.wufazhuce.com/one'
-	Apiurl = 'http://m.wufazhuce.com/one/ajaxlist/0?_token='
